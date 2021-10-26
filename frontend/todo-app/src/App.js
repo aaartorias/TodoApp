6c3e66c5dfa97ad492d46eab8d0829be0b739e80
shubhendu-repo <rajputshubhendu@gmail.com>
2021-10-26 21:20:54 +0530

    imported bootstrap.min.css from link and formatted minor styling

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import FirstComponent from './components/learning-examples/firstComponent';
 import SecondComponent from './components/learning-examples/secondComponent'; 
 import {ThirdComponent} from './components/learning-examples/thirdComponent'; // for non-default exports we need to use curly braces
- import TodoApp from './components/todo/TodoApp';
+import TodoApp from './components/todo/TodoApp';
+import './bootstrap.css';
 
 class App extends Component {
   render() {
--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -7,13 +7,16 @@ class TodoApp extends Component {
         return (
             <div className="TodoApp">
                 <Router>
+                    <Header />
                     <Switch>    
                         <Route path="/" exact component={LoginComponent}></Route>
                         <Route path="/login" component={LoginComponent}></Route>
                         <Route path="/welcome/:name" component={WelcomeComponent}></Route>
                         <Route path="/todos" component={ListToDosComponent}></Route>
+                        <Route path="/logout" component={LogoutComponent}></Route>
                         <Route path="" component={ErrorComponent}></Route>
-                    </Switch>    
+                    </Switch>
+                    <Footer/>    
                 </Router>
             </div>
         )
@@ -41,17 +44,16 @@ class LoginComponent extends Component {
         );
 
     }
-
+    
+    // history - lets us manage session history anythwere JS runs
+    // history - manage history stack, navigate, confirm navigation and persist state between sessions
     loginClicked = () => {
         // Hardcoding the login credentials: At present only {JohnDoe and dummy} are correct credentials
         // console.log(this.state)
         if (this.state.username==='JohnDoe' && this.state.password==='dummy') {
             console.log(this.state)
-            // history - lets us manage session history anythwere JS runs
-            // history - manage history stack, navigate, confirm navigation and persist state between sessions
-            this.props.history.push(`/welcome/${this.state.username}`)
-            // this.setState({loginSucceded: true})
-            // this.setState({loginFailed: false})
+             this.props.history.push(`/welcome/${this.state.username}`)
+            
         } else {
             console.log('failed')
             this.setState({loginFailed: true})
@@ -129,6 +131,53 @@ class ListToDosComponent extends Component {
                         
                     </tbody>              
                 </table>
+            </div>
+        )
+    }
+}
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div>
+                        <a className="navbar-brand" href="www.google.com">Google Search</a>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li><Link  className="nav-link" to="/welcome/JohnDoe">Home</Link> </li>
+                        <li><Link  className="nav-link" to="/todos">Todos</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li><Link  className="nav-link" to="/login">Login</Link></li>
+                        <li><Link  className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+
+                </nav>
+            </header>
+            
+        )
+    }
+}
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <span className="text-muted"> All rights Reserved 2021 @shuhbhendu</span>
+            </footer>
+        )
+    }
+}
+
+class LogoutComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>You are logged out</h1>
+                <div className="container">
+                    Thanks for using our application
+                </div>
             </div>
         )
     }
