a3bd2cfa7d5d92c610b9f1f0f432438ae22c3f64
shubhendu-repo <rajputshubhendu@gmail.com>
2021-10-26 21:59:56 +0530

    enabled deregistration of user from sessionStorage

--- frontend/todo-app/src/components/todo/AuthenticationService.js
+++ frontend/todo-app/src/components/todo/AuthenticationService.js
@@ -1,8 +1,15 @@
-
+// register that a user has logged in  using sessionStorage.setItem()
+// data stored in sessionStorage gets cleared when the page session ends
+// data in localStorage has no expiration time
 class AuthenticationService {
+    
     registerSuccessfulLogin(username,password) {
         console.log('register successful login');
         sessionStorage.setItem('authenticatedUser', username);
+    }
+
+    logout() {
+        sessionStorage.removeItem('authenticatedUser');
     }
     
 }
