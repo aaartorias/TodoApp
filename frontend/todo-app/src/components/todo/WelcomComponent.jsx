d0bb9d6be935a767374f98faddfc103d460bf5be
shubhendu-repo <rajputshubhendu@gmail.com>
2021-10-28 21:33:11 +0530

    frontend: updated server listening port to 4200 and added dummy welcome message

--- frontend/todo-app/src/components/todo/WelcomComponent.jsx
+++ frontend/todo-app/src/components/todo/WelcomComponent.jsx
@@ -1,7 +1,12 @@
 import React, {Component} from "react";
 import {BrowserRouter as Link} from 'react-router-dom';
 
-class WelcomeComponent extends Component { 
+class WelcomeComponent extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return(
             <div>
@@ -9,8 +14,16 @@ class WelcomeComponent extends Component {
                 <div className="container">
                     Welcome {this.props.match.params.name}. You can manage your todo list <Link to='/todos'>here</Link>
                 </div>
+                <div className="container">
+                    Click here to get a customized welcome message 
+                    <button  onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+                </div>
             </div>
         ) 
+    }
+
+    retrieveWelcomeMessage = () => {
+        console.log("Retrieve welcome message called")
     }
 }
 
