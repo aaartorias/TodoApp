ff2ba145525b58c30c10e16b73500fb61e2a5865
shubhendu-repo <rajputshubhendu@gmail.com>
2021-10-28 22:48:39 +0530

    Allow Cross-Origin-Requests

--- backend/restful-web-services/restful-web-services/src/main/java/com/todo/webservices/restfulwebservices/HelloWorldController.java
+++ backend/restful-web-services/restful-web-services/src/main/java/com/todo/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.todo.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 // Controller
 // handle rest request
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 
 	//GET
